7ed7c0aca817caac8b444e468a15139a061168f0
galigoldman <gali.goldman@post.runi.ac.il>
2024-12-05 21:10:54 +0200

    fix

--- Primes.java
+++ Primes.java
@@ -2,7 +2,7 @@ public class Primes {
     public static void main(String[] args) {
         // Replace this statement with your code
         int n = Integer.parseInt(args[0]);
-        int i=0, p=2;
+        int i=0, p=2, count=0;
         boolean[] arr = new boolean[n+1];
         for(i=2; i<n+1; i++){
             arr[i] = true;
@@ -23,8 +23,9 @@ public class Primes {
         for(i=0 ; i<n+1 ; i++){
             if(arr[i]){
                 System.out.println(i);
+                count++;
             }
         }
-
+        System.out.println("There are " + count + "primes between 2 and "+ n +" ("+ (100.0*count/(n-1)) +"% are primes)");
     }
 }
